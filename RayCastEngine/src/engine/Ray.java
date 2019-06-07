99931f5af0f8914db430924b0fcbf98b4f6217fd
Antonio Ferreras <33009451+Antfs10@users.noreply.github.com>
2019-06-06 20:21:12 -0400

    Added new methods used for lighting

--- RayCastEngine/src/engine/Ray.java
+++ RayCastEngine/src/engine/Ray.java
@@ -4,7 +4,22 @@ import java.awt.Graphics2D;
 import java.awt.geom.Point2D;
 
 public class Ray {
-	static Point2D.Double getPointHor(Graphics2D g2d, double rx, double ry, double rdir) {
+	static int checkCollisionHor(double x, double y) {
+		int mapX = (int) (x/Game.cellWidth);
+		int mapY = (int) (y/Game.cellHeight);
+		
+		return Game.map[mapX][mapY];
+	}
+	
+	static int checkCollisionVert(double x, double y) {
+		int mapX = (int) (x/Game.cellWidth);
+		int mapY = (int) (y/Game.cellHeight);
+		
+		return Game.map[mapX][mapY];
+	}
+	
+	
+	static Point2D.Double getPointHor(double rx, double ry, double rdir) {
 		//Convert rdir to standard rotation angle
 		if(rdir < 0)
 			rdir = 360+rdir;
@@ -16,16 +31,16 @@ public class Ray {
 		
 		//Find point A and Ya
 		if(rdir > 0 && rdir < 180) {
-			ay = Math.floor(Game.camPos.y/Game.cellHeight) * Game.cellHeight - 1;
+			ay = Math.floor(ry/Game.cellHeight) * Game.cellHeight - 0.001d;
 			ya = -Game.cellHeight;
 		} else if(rdir> 180 && rdir < 360) {
-			ay = Math.floor(Game.camPos.y/Game.cellHeight) * Game.cellHeight + Game.cellHeight;
+			ay = Math.floor(ry/Game.cellHeight) * Game.cellHeight + Game.cellHeight;
 			ya = Game.cellHeight;
 		} else {
-			ay = Game.camPos.y;
+			ay = ry;
 			ya = 0;
 		}
-		ax = Game.camPos.x + (Game.camPos.y-ay)/Math.tan(Math.toRadians(rdir));
+		ax = rx + (ry-ay)/Math.tan(Math.toRadians(rdir));
 		
 		//Finding Xa
 		if(0 < rdir && rdir < 180) 
@@ -44,7 +59,7 @@ public class Ray {
 			return new Point2D.Double(Integer.MAX_VALUE, Integer.MAX_VALUE);
 		
 		//Do collision check
-		if(Game.map[mapX][mapY] > 0 && Game.map[mapX][mapY] != 3) {
+		if(checkCollisionHor(ax, ay) > 0 && checkCollisionHor(ax, ay) != 3) {
 			return new Point2D.Double(ax, ay);
 		}
 		
@@ -69,7 +84,7 @@ public class Ray {
 				break;
 			
 			//Check for collision
-			if(Game.map[mapX][mapY] > 0 && Game.map[mapX][mapY] != 3) {
+			if(checkCollisionHor(x, y) > 0 && checkCollisionHor(x, y) != 3) {
 				return new Point2D.Double(x, y);
 			}
 		}
@@ -77,7 +92,7 @@ public class Ray {
 		return new Point2D.Double(Integer.MAX_VALUE, Integer.MAX_VALUE);
 	}
 	
-	static Point2D.Double getPointVert(Graphics2D g2d, double rx, double ry, double rdir) {
+	static Point2D.Double getPointVert(double rx, double ry, double rdir) {
 		//Convert rdir to standard rotation angle
 		if(rdir < 0)
 			rdir = 360+rdir;
@@ -89,16 +104,16 @@ public class Ray {
 		
 		//Find point A and Xa
 		if(rdir < 90 || rdir > 270) {
-			ax = Math.floor(Game.camPos.x/Game.cellWidth) * Game.cellWidth + Game.cellWidth;
+			ax = Math.floor(rx/Game.cellWidth) * Game.cellWidth + Game.cellWidth;
 			xa = Game.cellHeight;
 		} else if(rdir > 90 || rdir < 270) {
-			ax = Math.floor(Game.camPos.x/Game.cellWidth) * Game.cellWidth - 1;
+			ax = Math.floor(rx/Game.cellWidth) * Game.cellWidth - 0.001d;
 			xa = -Game.cellHeight;
 		} else {
-			ax = Game.camPos.x;
+			ax = rx;
 			xa = 0;
 		}
-		ay = Game.camPos.y + (Game.camPos.x-ax)*Math.tan(Math.toRadians(rdir));
+		ay = ry + (rx-ax)*Math.tan(Math.toRadians(rdir));
 		
 		//Finding Ya
 		if(rdir < 90 || rdir > 270) 
@@ -117,7 +132,7 @@ public class Ray {
 			return new Point2D.Double(Integer.MAX_VALUE, Integer.MAX_VALUE);
 		
 		//Collision check
-		if(Game.map[mapX][mapY] > 0 && Game.map[mapX][mapY] != 3) {
+		if(checkCollisionVert(ax, ay) > 0 && checkCollisionVert(ax, ay) != 3) {
 			return new Point2D.Double(ax, ay);
 		}
 		
@@ -141,7 +156,7 @@ public class Ray {
 				break;
 			
 			//Check for collision
-			if(Game.map[mapX][mapY] > 0 && Game.map[mapX][mapY] != 3) {
+			if(checkCollisionVert(x, y) > 0 && checkCollisionVert(x, y) != 3) {
 				return new Point2D.Double(x, y);
 			}
 		}
@@ -149,7 +164,7 @@ public class Ray {
 		return new Point2D.Double(Integer.MAX_VALUE, Integer.MAX_VALUE);
 	}
 	
-	static Point2D.Double getPointHorTall(Graphics2D g2d, double rx, double ry, double rdir) {
+	static Point2D.Double getPointHorTall(double rx, double ry, double rdir) {
 		//Convert rdir to standard rotation angle
 		if(rdir < 0)
 			rdir = 360+rdir;
@@ -161,16 +176,16 @@ public class Ray {
 		
 		//Find point A and Ya
 		if(rdir > 0 && rdir < 180) {
-			ay = Math.floor(Game.camPos.y/Game.cellHeight) * Game.cellHeight - 1;
+			ay = Math.floor(ry/Game.cellHeight) * Game.cellHeight - 0.001d;
 			ya = -Game.cellHeight;
 		} else if(rdir> 180 && rdir < 360) {
-			ay = Math.floor(Game.camPos.y/Game.cellHeight) * Game.cellHeight + Game.cellHeight;
+			ay = Math.floor(ry/Game.cellHeight) * Game.cellHeight + Game.cellHeight;
 			ya = Game.cellHeight;
 		} else {
-			ay = Game.camPos.y;
+			ay = ry;
 			ya = 0;
 		}
-		ax = Game.camPos.x + (Game.camPos.y-ay)/Math.tan(Math.toRadians(rdir));
+		ax = rx + (ry-ay)/Math.tan(Math.toRadians(rdir));
 		
 		//Finding Xa
 		if(0 < rdir && rdir < 180) 
@@ -179,7 +194,7 @@ public class Ray {
 			xa = -Game.cellHeight/Math.tan(Math.toRadians(rdir));
 		
 		//Distance from a to ray
-		//double dist = distance(Game.camPos.x, Game.camPos.y, ax, ay);
+		//double dist = distance(rx, ry, ax, ay);
 		
 		//Check if A touches wall
 		int mapX = (int) (ax/Game.cellWidth);
@@ -224,7 +239,7 @@ public class Ray {
 		return new Point2D.Double(Integer.MAX_VALUE, Integer.MAX_VALUE);
 	}
 	
-	static Point2D.Double getPointVertTall(Graphics2D g2d, double rx, double ry, double rdir) {
+	static Point2D.Double getPointVertTall(double rx, double ry, double rdir) {
 		//Convert rdir to standard rotation angle
 		if(rdir < 0)
 			rdir = 360+rdir;
@@ -236,16 +251,16 @@ public class Ray {
 		
 		//Find point A and Xa
 		if(rdir < 90 || rdir > 270) {
-			ax = Math.floor(Game.camPos.x/Game.cellWidth) * Game.cellWidth + Game.cellWidth;
+			ax = Math.floor(rx/Game.cellWidth) * Game.cellWidth + Game.cellWidth;
 			xa = Game.cellHeight;
 		} else if(rdir > 90 || rdir < 270) {
-			ax = Math.floor(Game.camPos.x/Game.cellWidth) * Game.cellWidth - 1;
+			ax = Math.floor(rx/Game.cellWidth) * Game.cellWidth - 0.001d;
 			xa = -Game.cellHeight;
 		} else {
-			ax = Game.camPos.x;
+			ax = rx;
 			xa = 0;
 		}
-		ay = Game.camPos.y + (Game.camPos.x-ax)*Math.tan(Math.toRadians(rdir));
+		ay = ry + (rx-ax)*Math.tan(Math.toRadians(rdir));
 		
 		//Finding Ya
 		if(rdir < 90 || rdir > 270) 
@@ -289,6 +304,151 @@ public class Ray {
 			
 			//Check for collision
 			if(Game.map[mapX][mapY] == 3) {
+				return new Point2D.Double(x, y);
+			}
+		}
+		
+		return new Point2D.Double(Integer.MAX_VALUE, Integer.MAX_VALUE);
+	}
+	
+	static Point2D.Double getPointHorAll(double rx, double ry, double rdir) {
+		//Convert rdir to standard rotation angle
+		if(rdir < 0)
+			rdir = 360+rdir;
+		else if (rdir > 360) 
+			rdir = rdir-360;
+		
+		double ay, ax;
+		double xa, ya;
+		
+		//Find point A and Ya
+		if(rdir > 0 && rdir < 180) {
+			ay = Math.floor(ry/Game.cellHeight) * Game.cellHeight - 0.001d;
+			ya = -Game.cellHeight;
+		} else if(rdir> 180 && rdir < 360) {
+			ay = Math.floor(ry/Game.cellHeight) * Game.cellHeight + Game.cellHeight;
+			ya = Game.cellHeight;
+		} else {
+			ay = ry;
+			ya = 0;
+		}
+		ax = rx + (ry-ay)/Math.tan(Math.toRadians(rdir));
+		
+		//Finding Xa
+		if(0 < rdir && rdir < 180) 
+			xa = Game.cellHeight/Math.tan(Math.toRadians(rdir));
+		else
+			xa = -Game.cellHeight/Math.tan(Math.toRadians(rdir));
+		
+		//Check if A touches wall
+		int mapX = (int) (ax/Game.cellWidth);
+		int mapY = (int) (ay/Game.cellHeight);
+		
+		////Prevent using A point outside map
+		if(mapX > Game.gridWidth-1 || mapX < 0)
+			return new Point2D.Double(Integer.MAX_VALUE, Integer.MAX_VALUE);
+		if(mapY > Game.gridHeight-1 || mapY < 0)
+			return new Point2D.Double(Integer.MAX_VALUE, Integer.MAX_VALUE);
+		
+		//Do collision check
+		if(checkCollisionHor(ax, ay) > 0) {
+			return new Point2D.Double(ax, ay);
+		}
+		
+		
+		
+		//Finding all other points where ray crosses horizontal grid lines
+		//If points touches wall, return the distance
+		double x = ax;
+		double y = ay;
+		int maxSteps = 20;
+		for(int i = 0; i < maxSteps; i++) {
+			x += xa;
+			y += ya;
+			
+			mapX = (int) (x/Game.cellWidth);
+			mapY = (int) (y/Game.cellHeight);
+			
+			//Prevent using point outside map
+			if(mapX > Game.gridWidth-1 || mapX < 0)
+				break;
+			if(mapY > Game.gridHeight-1 || mapY < 0)
+				break;
+			
+			//Check for collision
+			if(checkCollisionHor(x, y) > 0) {
+				return new Point2D.Double(x, y);
+			}
+		}
+		
+		return new Point2D.Double(Integer.MAX_VALUE, Integer.MAX_VALUE);
+	}
+	
+	static Point2D.Double getPointVertAll(double rx, double ry, double rdir) {
+		//Convert rdir to standard rotation angle
+		if(rdir < 0)
+			rdir = 360+rdir;
+		else if (rdir > 360) 
+			rdir = rdir-360;
+		
+		double ay, ax;
+		double xa, ya;
+		
+		//Find point A and Xa
+		if(rdir < 90 || rdir > 270) {
+			ax = Math.floor(rx/Game.cellWidth) * Game.cellWidth + Game.cellWidth;
+			xa = Game.cellHeight;
+		} else if(rdir > 90 || rdir < 270) {
+			ax = Math.floor(rx/Game.cellWidth) * Game.cellWidth - 0.001d;
+			xa = -Game.cellHeight;
+		} else {
+			ax = rx;
+			xa = 0;
+		}
+		ay = ry + (rx-ax)*Math.tan(Math.toRadians(rdir));
+		
+		//Finding Ya
+		if(rdir < 90 || rdir > 270) 
+			ya = -Game.cellHeight*Math.tan(Math.toRadians(rdir));
+		else
+			ya = Game.cellHeight*Math.tan(Math.toRadians(rdir));
+		
+		//Check if A touches wall
+		int mapX = (int) (ax/Game.cellWidth);
+		int mapY = (int) (ay/Game.cellHeight);
+		
+		//Prevent using A point outside map
+		if(mapX > Game.gridWidth-1 || mapX < 0)
+			return new Point2D.Double(Integer.MAX_VALUE, Integer.MAX_VALUE);
+		if(mapY > Game.gridHeight-1 || mapY < 0)
+			return new Point2D.Double(Integer.MAX_VALUE, Integer.MAX_VALUE);
+		
+		//Collision check
+		if(checkCollisionVert(ax, ay) > 0) {
+			return new Point2D.Double(ax, ay);
+		}
+		
+		
+		//Finding all other points where ray crosses horizontal grid lines
+		//If points touches wall, return the distance
+		double x = ax;
+		double y = ay;
+		int maxSteps = 20;
+		for(int i = 0; i < maxSteps; i++) {
+			x += xa;
+			y += ya;
+			
+			mapX = (int) (x/Game.cellWidth);
+			mapY = (int) (y/Game.cellHeight);
+			
+			//Prevent using point outside map
+			if(mapX > Game.gridWidth-1 || mapX < 0)
+				break;
+			if(mapY > Game.gridHeight-1 || mapY < 0)
+				break;
+			
+			//Check for collision
+			if(checkCollisionVert(x, y) > 0) {
 				return new Point2D.Double(x, y);
 			}
 		}
@@ -298,6 +458,12 @@ public class Ray {
 	
 	public static double distance(Point2D.Double one, Point2D.Double two) {       
 		return Math.sqrt((two.y - one.y) * (two.y - one.y) + (two.x - one.x) * (two.x - one.x));
+	}
+	
+	public static double direction(Point2D.Double one, Point2D.Double two) {
+	    double angle = -Math.toDegrees(Math.atan2(one.y - two.y, one.x - two.x)) + 180;
+
+	    return angle;
 	}
 	
 	public static double squaredDistance(Point2D.Double one, Point2D.Double two) {       
